cb231ee02dcf2d960c0d59a647ae21d28a5825b6
lookingforni <lookingforni@163.com>
2014-12-05 13:59:08 +0800

    add

---
+++ frontend/views/index/index.php
@@ -0,0 +1,20 @@
+<title>客客出品专业威客系统</title>
+<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE,chrome=1">
+<meta name="keywords" content="客客出品专业威客系统">
+<meta name="description" content="客客出品专业威客系统">
+<meta name="generator" content="客客出品 2.2" />
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style” content=black" /> 
+<meta name="author" content="kekezu" />
+<meta name="copyright" content="Copyright &#169; 2010 -2013 kekezu. All rights reserved" />
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="apple-touch-icon" href="favicon.ico"/>
+<script type="text/javascript">
+var SITEURL= "http://www.weike.com",
+    SKIN_PATH = 'tpl/default',
+LANG       = 'cn',
+    INDEX      = 'index',
+    CHARSET    = "utf-8";
+</script>
+1223
