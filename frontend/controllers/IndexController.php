2b251c4078b011583697f53179088e3cd77ce643
a <1052746142@qq.com>
2014-12-05 09:29:55 +0800

    添加项目

---
+++ frontend/controllers/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
+/**
+ * Site controller
+ */
+class IndexController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+	public function actionIndex(){
+		echo 1;
+		//return $this->render('index');
+	}
+}
